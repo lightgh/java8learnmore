501cc1084dbdd34251f2e2a8c11314a87cdb927a
Light .I. Chinaka <chinakalight@googlemail.com>
2019-12-29 22:39:13 +0100

    added demo class declaration without generics

---
+++ src/com/chinakalight/generics/DemoClassFirst.java
@@ -0,0 +1,13 @@
+package com.chinakalight.generics;
+
+/**
+ * @author - Chinaka .I. Light <ichinaka@byteworks.com.ng>
+ * Date: 29/12/2019
+ */
+public class DemoClassFirst {
+    private Object t;
+
+    public void set(Object t) { this.t = t; }
+
+    public Object get(){ return t; }
+}
---
+++ src/com/chinakalight/generics/DemoClassFirstTest.java
@@ -0,0 +1,13 @@
+package com.chinakalight.generics;
+
+/**
+ * @author - Chinaka .I. Light <ichinaka@byteworks.com.ng>
+ * Date: 29/12/2019
+ */
+public class DemoClassFirstTest {
+    public static void main(String[] args) {
+        DemoClassFirst demoClassFirst = new DemoClassFirst();
+        demoClassFirst.set("Testing Demo Class - About String");
+        System.out.println(demoClassFirst.get());
+    }
+}
