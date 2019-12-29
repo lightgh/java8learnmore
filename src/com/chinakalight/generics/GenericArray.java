a9eadc4c2d9fb10303087fa1c88f73ba43058360
Light .I. Chinaka <chinakalight@googlemail.com>
2019-12-29 23:49:57 +0100

    cannot create a generic array of T

--- src/com/chinakalight/generics/GenericArray.java
+++ src/com/chinakalight/generics/GenericArray.java
@@ -10,5 +10,14 @@ public class GenericArray<T> {
     public T[] notYetInstantiatedArray;
 
     // causes compiler error; Cannot create a generic array of T
-    public T[] array = new T[5];
+//    public T[] array = new T[5];
+
+    public static void main(String[] args) {
+        GenericArray<String> a = new GenericArray<>();
+
+        Object[] objArr = new String[10];
+        objArr[0] = new String();
+        objArr[1] = "lowcash";
+//        objArr[2] = 10;
+    }
 }
